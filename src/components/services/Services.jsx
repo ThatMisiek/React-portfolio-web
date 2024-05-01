dd1703386790bce536e0a68ea8fb5a7400f9b4b4
ThatMisiek <kowal147@vp.pl>
2024-05-01 18:37:15 +0200

    stage 6

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,100 @@
 import React from "react";
 import "./services.css";
+import { TbZoomCheck } from "react-icons/tb";
 
 function Services() {
-	return <section id='services'>Services</section>;
+	return (
+		<section id='services'>
+			<h5>What I Can Do For You</h5>
+			<h2>Services</h2>
+
+			<div className='container services__container'>
+				<article className='service'>
+					<div className='service__head'>
+						<h3>UI/UX Design</h3>
+					</div>
+					<ul className='service__list'>
+						<li>
+							<TbZoomCheck className='service__list-icon' />
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+						</li>
+						<li>
+							<TbZoomCheck className='service__list-icon' />
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+						</li>
+						<li>
+							<TbZoomCheck className='service__list-icon' />
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+						</li>
+						<li>
+							<TbZoomCheck className='service__list-icon' />
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+						</li>
+						<li>
+							<TbZoomCheck className='service__list-icon' />
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+						</li>
+					</ul>
+				</article>
+
+				<article className='service'>
+					<div className='service__head'>
+						<h3>Web Development</h3>
+					</div>
+					<ul className='service__list'>
+						<li>
+							<TbZoomCheck className='service__list-icon' />
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+						</li>
+						<li>
+							<TbZoomCheck className='service__list-icon' />
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+						</li>
+						<li>
+							<TbZoomCheck className='service__list-icon' />
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+						</li>
+						<li>
+							<TbZoomCheck className='service__list-icon' />
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+						</li>
+					</ul>
+				</article>
+
+				<article className='service'>
+					<div className='service__head'>
+						<h3>Content Creator</h3>
+					</div>
+					<ul className='service__list'>
+						<li>
+							<TbZoomCheck className='service__list-icon' />
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+						</li>
+						<li>
+							<TbZoomCheck className='service__list-icon' />
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+						</li>
+						<li>
+							<TbZoomCheck className='service__list-icon' />
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+						</li>
+						<li>
+							<TbZoomCheck className='service__list-icon' />
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+						</li>
+						<li>
+							<TbZoomCheck className='service__list-icon' />
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+						</li>
+						<li>
+							<TbZoomCheck className='service__list-icon' />
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+						</li>
+					</ul>
+				</article>
+			</div>
+		</section>
+	);
 }
 
 export default Services;
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -2,7 +2,20 @@ import React from "react";
 import "./testimonials.css";
 
 function Testimonials() {
-	return <div>Testimonials</div>;
+	return (
+		<section>
+			<h5>Review Form Clients</h5>
+			<h2>Testimonials</h2>
+
+			<div className="container tesimonials__container">
+				<article className="testimonials">
+					<div className="client__avatar">
+						<img src="" alt="" />
+					</div>
+				</article>
+			</div>
+		</section>
+	);
 }
 
 export default Testimonials;
